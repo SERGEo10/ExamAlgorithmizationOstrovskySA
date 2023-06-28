501aa5253a8f68f59f66cbf927792eb4ffb235a5
SERGEo <106819250+SERGEo10@users.noreply.github.com>
2023-06-28 11:51:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.Write(value: "Введите фамилию: ");
+    string surname = Console.ReadLine();
+
+    Console.Write("Введите желаемую оценку: ");
+    string grade = Console.ReadLine();
+
+    Console.WriteLine($"Поздравляю, {surname}! У Вас автоматом {grade} за экзамен.");
+    }
+}
+    
